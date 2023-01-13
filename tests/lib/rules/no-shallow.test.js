d7d7b40ce2744413412281dcb75b75ba11efc553
Ildar Sharafeev <40214328+sr-shifu@users.noreply.github.com>
2023-01-12 23:01:29 -0800

    Introduce rule to dissallow shadow rendering

---
+++ tests/lib/rules/no-shallow.test.js
@@ -0,0 +1,144 @@
+"use strict";
+
+const rule = require("../../../lib/rules/no-shallow");
+const RuleTester = require("eslint").RuleTester;
+
+const parserOptions = {
+  ecmaVersion: 2018,
+  sourceType: "module",
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+const VALID_PATTERNS = [
+  'const shallow = () => console.log("HelloWorld!")',
+  "import { mount } from 'enzyme';",
+  "const { mount } = require('enzyme');",
+  "import enzymeApi from 'enzyme';",
+  "import * as enzymeApi from 'enzyme';",
+  `
+    const enzymeApi = require('enzyme');
+    it('should render component', () => {
+        const wrapper = enzymeApi.mount(<MyReactComp />);
+        expect(wrapper.emptyRender()).toBe(true);
+    });
+  `,
+  `
+    const { shallow, mount } = require('enzyme');
+    it('should render component', () => {
+        const wrapper = mount(<MyReactComp />);
+        expect(wrapper.emptyRender()).toBe(true);
+    });
+  `,
+  `
+    import {shallow, mount} from 'enzyme';
+    it('should render component', () => {
+        const wrapper = mount(<MyReactComp />);
+        expect(wrapper.emptyRender()).toBe(true);
+    });
+  `,
+  `
+    const shallow = () => 'test';
+    it('should render component', () => {
+        const wrapper = shallow(<MyReactComp />);
+        expect(wrapper.emptyRender()).toBe(true);
+    });
+  `,
+];
+
+const INVALID_PATTERNS = {
+  noShallowCall: [
+    `
+      const enzymeApi = require('enzyme');
+      it('should render component', () => {
+          const wrapper = enzymeApi.shallow(<MyReactComp />);
+          expect(wrapper.emptyRender()).toBe(true);
+      });
+    `,
+    `
+      const { shallow } = require('enzyme');
+      it('should render component', () => {
+          const wrapper = shallow(<MyReactComp />);
+          expect(wrapper.emptyRender()).toBe(true);
+      });
+    `,
+    `
+      const { shallow, mount } = require('enzyme');
+      it('should render component', () => {
+          const wrapper = shallow(<MyReactComp />);
+          expect(wrapper.emptyRender()).toBe(true);
+      });
+    `,
+    `
+      import {shallow} from 'enzyme';
+      it('should render component', () => {
+          const wrapper = shallow(<MyReactComp />);
+          expect(wrapper.emptyRender()).toBe(true);
+      });
+    `,
+    `
+      import {shallow, mount} from 'enzyme';
+      it('should render component', () => {
+          const wrapper = shallow(<MyReactComp />);
+          expect(wrapper.emptyRender()).toBe(true);
+      });
+    `,
+    `
+      import * as enzymeApi from 'enzyme';
+      it('should render component', () => {
+          const wrapper = enzymeApi.shallow(<MyReactComp />);
+          expect(wrapper.emptyRender()).toBe(true);
+      });
+    `,
+    `
+      import enzymeApi from 'enzyme';
+      it('should render component', () => {
+          const wrapper = enzymeApi.shallow(<MyReactComp />);
+          expect(wrapper.emptyRender()).toBe(true);
+      });
+    `,
+  ],
+};
+
+const ruleTester = new RuleTester({ parserOptions });
+ruleTester.run("no-shallow-render", rule, {
+  valid: VALID_PATTERNS.map((pattern) => ({ code: pattern })),
+
+  invalid: Object.entries(INVALID_PATTERNS).flatMap(([errorId, patterns]) =>
+    patterns.map((pattern) => ({
+      code: pattern,
+      errors: [{ messageId: errorId }],
+    }))
+  ),
+});
+
+ruleTester.run("no-shallow-render", rule, {
+  valid: [
+    `
+        // defined in global scope outside of this module
+        // global.shallow = require('enzyme').shallow;
+        it('should render component', () => {
+            const wrapper = shallow(<MyReactComp />);
+            expect(wrapper.emptyRender()).toBe(true);
+        });
+      `,
+  ].map((pattern) => ({
+    code: pattern,
+    options: [{ resolveAsGlobal: false }],
+  })),
+  invalid: [
+    `
+      // defined in global scope outside of this module
+      // global.shallow = require('enzyme').shallow;
+      it('should render component', () => {
+          const wrapper = shallow(<MyReactComp />);
+          expect(wrapper.emptyRender()).toBe(true);
+      });
+    `,
+  ].map((pattern) => ({
+    code: pattern,
+    options: [{ resolveAsGlobal: true }],
+    errors: [{ messageId: "noShallowCall" }],
+  })),
+});
