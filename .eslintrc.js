87a40e3e0ec3ccba92e52ad1a34287a6e1650204
Ildar Sharafeev <40214328+sr-shifu@users.noreply.github.com>
2023-01-12 23:38:16 -0800

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
+  },
+  extends: ["eslint:recommended"],
+  plugins: [],
+  rules: {},
+};
